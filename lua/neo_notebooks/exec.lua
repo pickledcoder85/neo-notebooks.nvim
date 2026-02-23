87003efd134f3ea5651924b9f730ba0a0da342ca
Brian <brian@example.com>
2026-02-22 19:17:51 -0500

    Remove rich ANSI debug marker

--- lua/neo_notebooks/exec.lua
+++ lua/neo_notebooks/exec.lua
@@ -112,8 +112,6 @@ def _render_pandas_table(value, out_buf):
         table.add_row(*cells)
 
     console = Console(file=out_buf, force_terminal=True, color_system="standard")
-    if globals_dict.get("__neo_notebooks_debug_ansi", False):
-        out_buf.write("\x1b[35m[RICH ANSI ON]\x1b[0m\n")
     console.print(table)
     return True
 
@@ -144,8 +142,6 @@ def handle(obj):
                             print(repr(value))
                     elif use_rich and RICH_AVAILABLE:
                         console = Console(file=out_buf, force_terminal=True, color_system="standard")
-                        if globals_dict.get("__neo_notebooks_debug_ansi", False):
-                            out_buf.write("\x1b[35m[RICH ANSI ON]\x1b[0m\n")
                         console.print(value)
                     else:
                         if _is_pandas_obj(value):
